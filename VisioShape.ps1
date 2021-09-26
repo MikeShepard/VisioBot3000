516ae66ceb6d595a89f50b11b076e82851ed1390
mc1903 <mc1903@users.noreply.github.com>
2021-09-26 16:34:37 +0100

    Initial Commit of New-VisioRackShape
    Updated VisioShape.ps1 to add the new function New-VisioRackShape 
    Updated VisioBot3000.psd1 FunctionsToExport to include New-VisioRackShape

--- VisioShape.ps1
+++ VisioShape.ps1
@@ -130,3 +130,202 @@ Function Get-VisioShape{
     Param([string]$Name)
     $script:Shapes[$Name]
 }
+
+<#
+        .NOTES
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        Function:      New-VisioRackShape
+        Created by:    Martin Cooper
+        Date:          26/09/2021
+        GitHub:        https://github.com/mc1903
+        Version:       1.1
+
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        .SYNOPSIS
+        Drops and connects a new rack equipment shape (server, etc) into an existing rack shape.
+
+        .DESCRIPTION
+        Drops and connects a new rack equipment shape (server, etc) into an existing rack shape at a specified first (lowest) U space.
+
+        .PARAMETER Master
+        Either the name of the new rack equipment shape master (previously registered using Register-VisioShape) or a reference to a master object.
+
+        .PARAMETER Label
+        The label for the new rack equipment shape.
+
+        .PARAMETER Name
+        The name for the new rack equipment shape. If not provided then the Label will be used as the Name
+
+        .PARAMETER RackLabel
+        The name for the existing rack shape.
+
+        .PARAMETER RackVendor
+        The vendor name for the existing rack shape.
+
+        Supported Rack Vendors are HPE, Dell, IBM & Cisco.
+
+        Within each vendor only the shapes listed below are working.
+
+            HPE shapes from the HPE-Racks set on VisioCafe - https://www.visiocafe.com/downloads/hp/HPE-Common.zip
+
+                    HPE 22U G2 Adv Rack Front
+                    HPE 22U G2 Adv Rack Rear
+                    HPE 36U G2 Adv Rack Front
+                    HPE 36U G2 Adv Rack Rear
+                    HPE 42U 800mm G2 Adv Rack Front
+                    HPE 42U 800mm G2 Adv Rack Rear
+                    HPE 42U 800mm G2 Ent Rack Front
+                    HPE 42U 800mm G2 Ent Rack Rear
+                    HPE 42U G2 Adv Rack Front
+                    HPE 42U G2 Adv Rack Rear
+                    HPE 42U G2 Ent Rack Front
+                    HPE 42U G2 Ent Rack Rear
+                    HPE 48U 800mm G2 Adv Rack Front
+                    HPE 48U 800mm G2 Adv Rack Rear
+                    HPE 48U 800mm G2 Ent Rack Front
+                    HPE 48U 800mm G2 Ent Rack Rear
+                    HPE 48U G2 Adv Rack Front
+                    HPE 48U G2 Adv Rack Rear
+                    HPE 48U G2 Ent Rack Front
+                    HPE 48U G2 Ent Rack Rear
+                    50U Ent. Rack
+
+            Dell shapes from the Dell-Racks set on VisioCafe - https://www.visiocafe.com/downloads/dell/Dell-Racks.zip
+
+                    2420 Rack Frame
+                    4220 Rack Frame
+                    4220D Rack Frame
+                    4220W Rack Frame
+                    4820 Rack Frame
+                    4820D Rack Frame
+                    4820W Rack Frame
+
+            IBM shapes from the IBM-Racks set on VisioCafe - https://www.visiocafe.com/downloads/ibm/IBM-Common.zip
+
+                    7014-S11 Rack
+                    7014-S25 Rack
+                    7014-S00 Rack
+                    7014-T00 Rack
+                    7014-T42 Rack
+
+            Cisco shapes from the Cisco R-Series set on Cisco.com - https://www.cisco.com/c/dam/assets/prod/visio/visio/racks-cisco-r-series.zip
+
+                    RACK2-UCS Front
+                    RACK2-UCS Rear
+                    RACK2-UCS2 Front
+                    RACK2-UCS2 Rear
+
+        .PARAMETER FirstU
+        The first (lowest) U space in which to place the new rack equipment shape.
+
+        .PARAMETER X
+        The X position used for initial new rack equipment shape placement (in inches) prior to connecting it to the existing rack shape. If not provided the default X position = 1
+
+        .PARAMETER Y
+        The Y position used for initial new rack equipment shape placement (in inches) prior to connecting it to the existing rack shape. If not provided the default Y position = 0.25
+
+        .OUTPUTS
+        Visio.Shape
+
+        .EXAMPLE
+        New-VisioRackShape -Master HPEDL180Gen108LFF -Label Server1 -RackLabel Rack01 -RackVendor HPE -FirstU 1
+#>
+Function New-VisioRackShape{
+    [CmdletBinding(SupportsShouldProcess=$True)]
+    Param (
+	    [Parameter(Mandatory=$true)]$Master,
+	    [Parameter(Mandatory=$true)]$Label,
+	    [Parameter(Mandatory=$false)]$Name,
+	    [Parameter(Mandatory=$true)]$RackLabel,
+	    [Parameter(Mandatory=$true)]$RackVendor,
+	    [Parameter(Mandatory=$true)]$FirstU,
+		[Parameter(Mandatory=$false)]$X,
+		[Parameter(Mandatory=$false)]$Y
+    )
+    If($PSCmdlet.ShouldProcess('Visio','Drop a new rack equipment shape and connect to an existing rack shape')){
+        If($Master -is [string]){
+            $Master=$script:Shapes[$Master]
+        }
+        If(!$Name){
+            $Name=$Label
+        }
+
+        If(!$X){
+            $X=1.00
+        }
+
+        If(!$Y){
+            $Y=0.25
+        }
+
+        $p=Get-VisioPage
+        $ExistingRackShape=$p.Shapes | Where-Object {$_.Name -eq $RackLabel}
+        If(!$ExistingRackShape){
+            Write-Verbose "Existing rack shape $RackLabel was NOT found on the active page $($p.Name). Skipping!"
+            Break
+        }
+        Else {
+            Write-Verbose "Existing rack shape $RackLabel was found on the active page $($p.Name)."
+            If ($RackVendor -match 'HPE') {
+                $FirstUBXY = $FirstU | ForEach-Object { $_.ToString("00") }
+                $FirstUEXY = $FirstU | ForEach-Object { $_.ToString("00") }
+                $BeginXY = "=PAR(PNT($RackLabel!Connections.U$($FirstUBXY)B.X,$RackLabel!Connections.U$($FirstUBXY)B.Y))"
+                $EndXY = "=PAR(PNT($RackLabel!Connections.U$($FirstUEXY)E.X,$RackLabel!Connections.U$($FirstUEXY)E.Y))"
+            }
+            ElseIf ($RackVendor -match 'Dell') {
+                $FirstUBXY = $FirstU*2+3
+                $FirstUEXY = $FirstU*2+4
+                $BeginXY = "=PAR(PNT($RackLabel!Connections.X$($FirstUBXY),$RackLabel!Connections.Y$($FirstUBXY)))"
+                $EndXY = "=PAR(PNT($RackLabel!Connections.X$($FirstUEXY),$RackLabel!Connections.Y$($FirstUEXY)))"
+            }
+            ElseIf ($RackVendor -match 'IBM') {
+                $FirstUBXY = $FirstU*2+3
+                $FirstUEXY = $FirstU*2+4
+                $BeginXY = "=PAR(PNT($RackLabel!Connections.X$($FirstUBXY),$RackLabel!Connections.Y$($FirstUBXY)))"
+                $EndXY = "=PAR(PNT($RackLabel!Connections.X$($FirstUEXY),$RackLabel!Connections.Y$($FirstUEXY)))"
+            }
+            ElseIf ($RackVendor -match 'Cisco') {
+                $FirstUBXY = $FirstU
+                $FirstUEXY = $FirstU
+                $BeginXY = "=PAR(PNT($RackLabel!Connections.Cab$($FirstUBXY)A.X,$RackLabel!Connections.Cab$($FirstUBXY)A.Y))"
+                $EndXY = "=PAR(PNT($RackLabel!Connections.Cab$($FirstUBXY)B.X,$RackLabel!Connections.Cab$($FirstUBXY)B.Y))"
+            }
+            Else {
+                Write-Verbose "Rack Vendor $RackVendor is NOT known/supported. Skipping!"
+                Break
+            }
+
+        }
+
+<#
+    @Mike - unsure where $updatemode is set and if I need to worry about it here?
+
+        if($updateMode){
+            $DroppedShape=$p.Shapes | Where-Object {$_.Name -eq $Label}
+        }
+#>
+
+        Write-Verbose "Rack Vendor $RackVendor"
+        Write-Verbose "BeginXY: $BeginXY"
+        Write-Verbose "  EndXY: $EndXY"
+
+        $DroppedShape=$p.Drop($Master.PSObject.BaseObject,$X,$Y)
+        $DroppedShape.Name=$Name
+        $DroppedShape.Text=$Label
+        $DroppedShape.Cells("BeginX").FormulaU = $BeginXY
+        $DroppedShape.Cells("BeginY").FormulaU = $BeginXY
+        $DroppedShape.Cells("EndX").FormulaU = $EndXY
+        $DroppedShape.Cells("EndY").FormulaU = $EndXY
+
+        New-Variable -Name $Name -Value $DroppedShape -Scope Global -Force
+        
+        If ($PSBoundParameters['Verbose']) {
+            Write-Output $DroppedShape
+        }
+        
+        $Script:LastDroppedObject=$DroppedShape
+    }
+
+}
